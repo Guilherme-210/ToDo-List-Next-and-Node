92be7c6ceceee09a269bdc57778dec81efd67d67
Guilherme Reis <guireis1164@gmail.com>
2025-06-17 15:42:51 -0300

    refactor(CreatedTask): Remove onTaskCreated prop from component call

--- frontend/src/app/createdtask/page.tsx
+++ frontend/src/app/createdtask/page.tsx
@@ -5,7 +5,7 @@ import FormTodo from "@/components/FormTodo"
 export default function CreatedTask() {
   return (
     <main className="grid grid-cols-1 gap-6 items-start justify-items-center p-8 pb-20 min-h-screen font-[family-name:var(--font-geist-sans)]">
-      <FormTodo onTaskCreated={() => {}} />
+      <FormTodo />
     </main>
   )
 }
